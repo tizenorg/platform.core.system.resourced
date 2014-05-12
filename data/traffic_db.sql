5f686043b1bfe743e692de9c9f1554e9f816e043
Przemyslaw Kedzierski <p.kedzierski@samsung.com>
2014-05-12 14:31:28 +0200

    Second part of datausage module.
    The datausage module is used for network traffic statistics.
    This commit adds common part of code of datausage module,
    a skeleton code for initialization, finalization and control.
    
    Change-Id: Ifef33c6752577888fde6a14a5b588846d21afff1
    Signed-off-by: Przemyslaw Kedzierski <p.kedzierski@samsung.com>

---
+++ data/traffic_db.sql
@@ -0,0 +1,72 @@
+PRAGMA journal_mode = PERSIST;
+
+ -- Information about counted traffic per time points.
+CREATE TABLE IF NOT EXISTS statistics (
+  binpath TEXT,
+  received BIGINT,
+  sent BIGINT,
+  time_stamp BIGINT,
+  iftype INT,
+  is_roaming INT,
+  hw_net_protocol_type INT,
+  PRIMARY KEY (binpath, time_stamp, iftype)
+);
+
+CREATE INDEX IF NOT EXISTS binpath_st_idx ON statistics(binpath, iftype);
+
+ -- Information about defined quotas.
+CREATE TABLE IF NOT EXISTS quotas (
+  binpath TEXT,
+  sent_quota BIGINT,
+  rcv_quota BIGINT,
+  snd_warning_threshold INT,
+  rcv_warning_threshold INT,
+  time_period BIGINT,
+  start_time BIGINT,
+  iftype INT,
+  roaming INT,
+  PRIMARY KEY(binpath, iftype, roaming)
+);
+
+CREATE INDEX IF NOT EXISTS binpath_qt_idx ON quotas(binpath, iftype);
+
+ -- Information about active quotas. One entry per quota time interval.
+ -- Time intervals abut but don't overlap.
+CREATE TABLE IF NOT EXISTS effective_quotas (
+  binpath TEXT,
+  sent_used_quota BIGINT,
+  rcv_used_quota BIGINT,
+  start_time BIGINT,
+  finish_time BIGINT,
+  iftype INT,
+  roaming INT,
+  state INT DEFAULT 0,
+  PRIMARY KEY (binpath, iftype, start_time, finish_time, roaming)
+);
+
+CREATE INDEX IF NOT EXISTS binpath_effective_quotas_idx ON effective_quotas(binpath, iftype);
+
+ -- Information about restrictions.
+CREATE TABLE IF NOT EXISTS restrictions (
+  binpath TEXT,
+  rcv_limit BIGINT,
+  send_limit BIGINT,
+  iftype INT,
+  rst_state INT,
+  quota_id INT,
+  roaming INT,
+  PRIMARY KEY (binpath, iftype)
+);
+
+CREATE INDEX IF NOT EXISTS binpath_restrictions_idx ON restrictions(binpath, iftype);
+
+ -- Information about network interfaces state
+CREATE TABLE IF NOT EXISTS iface_status (
+  update_time BIGINT,
+  iftype INT,
+  ifstatus INT,
+  PRIMARY KEY (update_time)
+);
+
+CREATE INDEX IF NOT EXISTS update_tm_if_idx ON iface_status(update_time, iftype, ifstatus);
+
