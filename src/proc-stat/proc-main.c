54a6dd62a9ec9c24e5bf7801165baddadf2e7cee
Prajwal A N <an.prajwal@samsung.com>
2015-01-15 12:42:07 +0900

    Changed casting expression for 64 bit arch.
    Casting pointer to int directly does not work in 64 bit environments. Thus changed the casting expression.
    Added a check to ensure that arguments passed to strstr function are not null.
    
    Change-Id: I4765d0503b65e080f0394481448bbd1b3050c08e
    Signed-off-by: Prajwal A N <an.prajwal@samsung.com>

--- src/proc-stat/proc-main.c
+++ src/proc-stat/proc-main.c
@@ -385,7 +385,10 @@ static void proc_free_exclude_key(gpointer data)
 
 static gboolean find_excluded(gpointer key, gpointer value, gpointer user_data)
 {
-	return (gboolean)strstr((char*)user_data, (char*)key);
+	if((char*)user_data && (char*)key)
+		return (strstr((char*)user_data, (char*)key) ? TRUE : FALSE);
+	else
+		return FALSE;
 }
 
 int resourced_proc_excluded(const char *app_name)
