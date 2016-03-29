efb022222846042b8a98d61db0c0e61a74f7c420
Kichan Kwon <k_c.kwon@samsung.com>
2016-03-29 12:16:04 +0900

    Return right internal storage root path
    Change-Id: I4909f7280628475dea892f4a7f41f26235325830
    Signed-off-by: Kichan Kwon <k_c.kwon@samsung.com>

--- src/common/storage-helper.c
+++ src/common/storage-helper.c
@@ -76,8 +76,16 @@ resourced_ret_c get_storage_root_path(int type, char **path)
 {
 	struct rd_storage target;
 
-	if (type == INTERNAL)
+	_I("Start getting %s root path",
+			(type == INTERNAL ? "internal" : "external"));
+
+	/* TODO : set user name according to current user */
+	if (type == INTERNAL) {
 		target.type = STORAGE_TYPE_INTERNAL;
+		strncpy(*path, "/home/owner/content", 20);
+		_I("Root path = %s", *path);
+		return RESOURCED_ERROR_NONE;
+	}
 	else if (type == EXTERNAL)
 		target.type = STORAGE_TYPE_EXTERNAL;
 	else {
@@ -85,9 +93,16 @@ resourced_ret_c get_storage_root_path(int type, char **path)
 		return RESOURCED_ERROR_INVALID_PARAMETER;
 	}
 
+	target.id = -1;
 	if (storage_foreach_device_supported(get_storage_id,
 				&target) != STORAGE_ERROR_NONE) {
 		_E("Failed to get storage ID");
+		return RESOURCED_ERROR_FAIL;
+	}
+
+	if(target.id == -1) {
+		_E("There is no %s storage",
+				(type == INTERNAL ? "internal" : "external"));
 		return RESOURCED_ERROR_FAIL;
 	}
 
@@ -97,6 +112,7 @@ resourced_ret_c get_storage_root_path(int type, char **path)
 		return RESOURCED_ERROR_FAIL;
 	}
 
+	_I("Root path = %s", *path);
 	return RESOURCED_ERROR_NONE;
 }
 
